d489cdafd97b80ff819243e9ebbc10ebbaec54ff
iSmo0ke <jesus.alt.2004@gmail.com>
2024-01-07 22:30:51 -0600

    Estilos

--- screens/users.js
+++ screens/users.js
@@ -5,17 +5,16 @@ import { StyleSheet, Text, View, Button, FlatList, TextInput } from 'react-nativ
 import { addUser, removeUser } from '../redux/features/users';
 
 export default function UsersScreen() {
-
     const dispatch = useDispatch();
     const list = useSelector((state) => state.users);
     const [value, setValue] = React.useState("");
 
-    const addItem = () =>{
-        if(value.length > 0){
+    const addItem = () => {
+        if (value.length > 0) {
             dispatch(addUser(value));
             setValue("");
-        }else{
-            alert("Campo vacio, ingresa una tarea");
+        } else {
+            alert("Campo vacío, ingresa una tarea");
         }
     };
 
@@ -23,101 +22,129 @@ export default function UsersScreen() {
         dispatch(removeUser(item));
     };
 
-    const renderItem = ({ item }) =>{
-        return(
-            <View style={({flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: 200})}>
+    const renderItem = ({ item }) => {
+        return (
+            <View style={styles.boxElement}>
                 <Text style={styles.text}>{item}</Text>
-                <Button color="red" title='Quitar' onPress={() => removeItem(item)}></Button>
+                <Button color="#14DE04" title='Completar' onPress={() => removeItem(item)}></Button>
             </View>
         );
     };
 
-    console.log(list)
-
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Mis tareas del dia</Text>
-            <View style={styles.addElement}>
-                <TextInput 
-                value={value}
-                placeholder='¿Cual es la tarea del dia?'
-                onChangeText={(item) => setValue(item)}
-                style={{
-                    width:250,
-                    borderBottomWidth: 1,
-                    borderBottomColor: "purple"
-                }}
-                />
-
-                <Button color="purple" title="Agregar" onPress={addItem}
-                />
+            <View style={styles.backgroundContainer}>
+            <Text style={styles.supTitle}>Mis tareas del día</Text>
             </View>
 
-            <Text style={styles.title}>Lista de tareas</Text>
-            
-            <View>
-                {
-                    list.length > 0 ?
+            <View style={styles.overlayContainer}>
+
+                <View style={styles.contentContainer}>
+                    <View style={styles.addElement}>
+                        <TextInput
+                            value={value}
+                            placeholder='¿Cuál es la tarea del día?'
+                            onChangeText={(item) => setValue(item)}
+                            style={{
+                                width: 250,
+                                borderBottomWidth: 1,
+                                borderBottomColor: "purple"
+                            }}
+                        />
+                        <Button color="#54B9FC" title="Agregar" onPress={addItem} />
+                    </View>
+
+                    <Text style={styles.title}>Lista de tareas</Text>
+
+                    {list.length > 0 ? (
                         <FlatList
-                            //style={({ width: "100%" })}
                             data={list}
-                            renderItem =  {renderItem}
+                            renderItem={renderItem}
                             keyExtractor={item => item}
                         />
-                        :
+                    ) : (
                         <Text style={styles.text}>No tienes tareas pendientes</Text>
-                }
+                    )}
+                </View>
             </View>
 
-         <StatusBar style="auto" />
+            <StatusBar style="auto" />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    supTitle: {
+        fontSize: 20,
+        color: "black",
+        fontStyle: "italic",
+        fontWeight: "bold",
+        padding: 50,
+        left: 50,
+        //textAlign: 'center',
+
+    },
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
-        //justifyContent: 'center',
-        width: "100%",
-        paddingTop: 50
+        paddingTop: 80,
     },
 
-    item: {
-        backgroundColor: "gray"
+    backgroundContainer: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '200%',
+        height: '200%',
+        backgroundColor: '#9263EA', 
+        
+    },
+
+    overlayContainer: {
+        flex: 0,
+        height: '98%',
+        width: '90%',
+        backgroundColor: 'white', 
+        borderRadius: 15, 
+        overflow: 'hidden',
+    },
+
+    contentContainer: {
+        padding: 20,
     },
 
     title: {
         fontSize: 20,
         color: "black",
         fontStyle: "italic",
-        fontWeight: "bold"
+        fontWeight: "bold",
+        marginBottom: 10,
+        textAlign: 'center', 
+        
+        
     },
 
     addElement: {
-        width: "100%",
-        paddingVertical:20,
         flexDirection: "row",
         justifyContent: "space-around",
-        alignItems: "center"
+        alignItems: "center",
+        marginBottom: 20,
     },
 
     text: {
         fontSize: 15,
         color: "black",
         fontStyle: "italic",
-        fontWeight: "bold"
+        fontWeight: "bold",
     },
 
     boxElement: {
-        width: "100%",
         borderBottomWidth: 1,
         borderBottomColor: "purple",
         backgroundColor: "white",
         padding: 20,
         flexDirection: "row",
         justifyContent: "space-between",
-        alignItems: "center"
-    }
+        alignItems: "center",
+    },
 });
